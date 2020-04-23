f59e841b8498bf0db6526da4606d4075cb2aa95d
raudelf <rawdell@outlook.com>
2020-04-23 16:37:31 -0500

    imported the auth router

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const authRouter = require('./auth/auth-router.js');
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
+server.use(cors());
+
+server.use('/api', authRouter);
+
+server.get('/api', (req, res) => {
+    res.json({ api: 'up' });
+});
 
 module.exports = server;
