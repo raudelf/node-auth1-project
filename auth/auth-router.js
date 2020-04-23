8b56d5faa4ccb5fac4f0709ba1863010841d3455
Raudel Flores <rawdell@outlook.com>
2020-04-23 18:08:44 -0500

    made username equal to res.session.user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,6 +23,7 @@ router.post('/login', (req, res) => {
   Users.findBy({username})
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = username;
         res.status(200).json({message: `Welcome ${username}!`})
       } else {
         res.status(401).json({message: 'Invalid Credentials'})
