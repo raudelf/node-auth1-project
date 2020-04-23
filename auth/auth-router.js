220bdbc440d1fbf5c56dc447e9761a25d959c07e
raudelf <rawdell@outlook.com>
2020-04-23 16:38:06 -0500

    created the register and login endpoints. Incorporated hash for passwords and created a few users in the process

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,12 +1,36 @@
+const bcrypt = require('bcryptjs');
+
 const router = require("express").Router();
 const Users = require("../endpoints/user-model.js");
 
-router.get("/", (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
+router.post("/register", (req, res) => {
+
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 16);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(creds => {
+      res.status(201).json({creds});
     })
-    .catch(err => res.send(err));
+    .catch(err => res.send({message: 'There was a problem with the db', err}));
 });
+
+router.post('/login', (req, res) => {
+  const {username, password} = req.body;
+
+  Users.findBy({username})
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({message: `Welcome ${username}!`})
+      } else {
+        res.status(401).json({message: 'Invalid Credentials'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'There was a problem with the db', err})
+    })
+})
 
 module.exports = router;
